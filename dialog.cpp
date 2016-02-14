acb8611ef72c91ec283bcc6c07874b8a7dafe959
DannyGotTea <danielgallagher500@gmail.com>
2016-02-14 08:10:46 -0800

    Position & Size Fixed
    the main window's position and size were fixed

--- dialog.cpp
+++ dialog.cpp
@@ -9,6 +9,11 @@ Dialog::Dialog(QWidget *parent) :
 {
     ui->setupUi(this);
     QWidget::setWindowFlags(Qt::CustomizeWindowHint | Qt::FramelessWindowHint);
+
+    QRect rec = QApplication::desktop()->screenGeometry();
+
+    this->setFixedSize(rec.width() * 0.7, rec.height() * 0.4);
+    this->move((rec.width() - this->width()) / 2 , (rec.height() - this->height()) * 0.9);
 }
 
 Dialog::~Dialog()
